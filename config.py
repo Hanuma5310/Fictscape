82ef42ed4aa3c87b74ace98cdcc53a404e4a7d7b
Marisetty Hanumantharao <124807924+Hanuma5310@users.noreply.github.com>
2025-05-05 01:09:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+DIALOGFLOW_AGENTS = {
+    "Monkey D. Luffy": {
+        "project_id": "fictscape-v01",
+        "agent_id": "7f9660aa-cf6f-4470-ae63-e5d0a68e9d8e",
+        "location": "us-central1"
+    },
+    "Tony Stark": {
+        "project_id": "fictscape-v01",
+        "agent_id": "297a5f15-1932-4f23-8248-1d51dcd682b2",
+        "location": "us-central1"
+    },
+    "Peter Parker": {
+        "project_id": "fictscape-v01",
+        "agent_id": "6c9dbf38-4b2a-40c6-8a5e-e00dd7518d73",
+        "location": "us-central1"
+    },
+    "Miles Morales": {
+        "project_id": "fictscape-v01",
+        "agent_id": "c0467b3a-b608-479d-b8b0-7ad4bdf5939e",
+        "location": "us-central1"
+    },
+    "Walter White": {
+        "project_id": "fictscape-v01",
+        "agent_id": "51113686-229a-472a-b44d-d24ac4996718",
+        "location": "us-central1"
+    },
+    "Naruto Uzumaki": {
+        "project_id": "fictscape-v01",
+        "agent_id": "abab7cc0-5c2e-462d-b87e-7616d98d6444",
+        "location": "us-central1"
+    },
+}
